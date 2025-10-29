373128c4a5f89c8cfab986b1353ec9a8c79b8b29
sunnyJha08 <sunnyjha98971@gmail.com>
2025-10-29 22:55:27 +0530

    Structuring the Project

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 function App() {
 
   return (
-    <h1>Hello, World!</h1>
+    <>
+      <h1 className="text-xl text-teal-600">Hello, World!</h1>
+    </>
   )
 }
 
-export default App
+export default App;
