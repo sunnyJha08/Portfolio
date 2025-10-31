a17f869561b8fbfbce7a8908357f8de3dc6c6e1e
sunnyJha08 <sunnyjha98971@gmail.com>
2025-10-31 21:34:27 +0530

    added hero section

---
+++ src/components/UI/Spacer.jsx
@@ -0,0 +1,6 @@
+export default function Spacer() {
+
+    return (
+        <div className="w-screen bg-amber-500  h-1/4"></div>
+    )
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,33 @@
 import Header from "../components/Header"
+import Spacer from "../components/UI/Spacer"
+
+const HeroSeection =() => {
+
+    return(
+        <section className="w-screen bg-blue-200  h-2/4 ">
+            <Spacer/>
+            <div className=" leading-tight text-[1.2rem] text-center p-4 h-1/2">
+                <p>Sunny Jha is an independent</p>
+                <p>software engineer his work is focused</p>
+                <p>on building scalable application.</p>
+                <p>He's commited to grow </p>
+                <p>personally and professionally</p>
+            </div>
+            <Spacer/>
+        </section>
+    )
+}
+
+
+
+
 
 export default function Home(){
 
     return(
-        <Header/>
+        <div className="w-screen bg-amber-950 h-screen">
+            <Header/>
+            <HeroSeection/>
+        </div>
     )
 }
