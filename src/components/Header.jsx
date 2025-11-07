01152b57a96bcbe2c569e0d39536e1a0bf58cd70
sunnyJha08 <sunnyjha98971@gmail.com>
2025-11-07 23:37:27 +0530

    Adding the Header component at the start of the home page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,28 @@
+import { Link } from "react-router-dom";
+
 const Header = () => {
-    return (
-        <p>This is a paragraph from the Header UI component from the /components/UI/...</p>
-    )
-}
+  return (
+    <div className="bg-red-700 max-w-full">
+      <nav className="flex">
+        <div className="w-2/4">
+          <a
+            className="w-max h-auto flex items-center gap-2 p-2"
+            href="http://localhost:5173/">
+            <img
+              src="../../../public/assets/logo.png"
+              alt="Logo"
+              className="h-auto max-w-[2.2rem]  rounded-full"
+            />
+            <p>Sunny Jha</p>
+          </a>
+        </div>
+        <div className="w-2/4 flex justify-end items-center gap-8 p-4 text-white">
+          <Link to="http://localhost:5173/blogs">Blogs</Link>
+          <button className="cursor-pointer">Contact</button>
+        </div>
+      </nav>
+    </div>
+  );
+};
 
 export default Header;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,9 @@
 import Header from "../components/Header";
 
 export default function Home() {
-  return <h1>Home page of the Portfolio</h1>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 }
