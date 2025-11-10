5b6d5fd9633d719a366a2d5e1fd7c67a3d87375e
sunnyJha08 <sunnyjha98971@gmail.com>
2025-11-10 21:55:16 +0530

    Fix: update border style of paragraphs in FeaturedBlogs component

--- src/components/UI/FeaturedBlogs.jsx
+++ src/components/UI/FeaturedBlogs.jsx
@@ -3,16 +3,16 @@ import LinkWithHoverText from "./LinkWithHoverText";
 export default function FeaturedBlogs() {
   return (
     <div className="mx-6 mb-8 h-auto max-w-screen">
-      <p className="my-6 border-b-4 border-black text-2xl font-extralight italic">
+      <p className="my-6 border-b-4 border-double border-black text-2xl font-extralight italic">
         <LinkWithHoverText href={"#"} hoverText={"Lorem ipsum dolor sit."} />
       </p>
-      <p className="my-6 border-b-4 border-black text-2xl font-extralight italic">
+      <p className="my-6 border-b-4 border-double border-black text-2xl font-extralight italic">
         <LinkWithHoverText href={"#"} hoverText={"Lorem ipsum dolor sit."} />
       </p>
-      <p className="my-6 border-b-4 border-black text-2xl font-extralight italic">
+      <p className="my-6 border-b-4 border-double border-black text-2xl font-extralight italic">
         <LinkWithHoverText href={"#"} hoverText={"Lorem ipsum dolor sit."} />
       </p>
-      <p className="my-6 border-b-4 border-black text-2xl font-extralight italic">
+      <p className="my-6 border-b-4 border-double border-black text-2xl font-extralight italic">
         <LinkWithHoverText href={"#"} hoverText={"Lorem ipsum dolor sit."} />
       </p>
     </div>
