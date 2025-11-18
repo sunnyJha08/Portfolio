a7f5db5c9d696a9962fb2bc42512c8c1fd4d737c
sunnyJha08 <sunnyjha98971@gmail.com>
2025-11-18 22:03:52 +0530

    feat: replace individual Inter font files with a variable font; update CSS to use InterVariable and adjust heading size on home page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <>
-      <h2 className="text-7xl font-bold">Home page</h2>
+      <h2 className="text-8xl font-bold">Home page</h2>
       <p>This uses Inter automatically.</p>
     </>
   );
