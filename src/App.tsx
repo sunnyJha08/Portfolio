d8dde45c6ca5f4903fbbcac42e6014062282e388
sunnyJha08 <sunnyjha98971@gmail.com>
2025-11-18 20:16:38 +0530

    feat: restructure app components; implement theme provider and mode toggle; update routing and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,15 @@
-// src/App.jsx
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import ContactForm from "./pages/ContactForm";
-import { Navbar } from "./components/Navbar";
-/*// Assuming you create these page components
-    import About from './pages/About';
- */
+import Contact from "./pages/Contact";
+import { Navbar } from "./components/ui/Navbar";
+
 function App() {
   return (
     <div>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/contact" element={<ContactForm />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </div>
   );
---
+++ src/components/ui/Navbar.tsx
@@ -0,0 +1,7 @@
+import { Link } from "react-router-dom";
+
+export function Navbar() {
+  return (
+    <div className="mx-auto flex max-w-xs items-center justify-between bg-(--background-color) p-2"></div>
+  );
+}
