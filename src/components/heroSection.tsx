b732ab43dd2b39050ebdf2bdc14584316ad0b480
sunnyJha08 <sunnyjha98971@gmail.com>
2025-11-20 23:27:38 +0530

    feat: update HeroSection heading size for improved visibility

--- src/components/heroSection.tsx
+++ src/components/heroSection.tsx
@@ -1,7 +1,7 @@
 export default function HeroSection() {
   return (
     <>
-      <h1 className="my-18 text-center text-lg leading-5 font-medium tracking-tighter text-neutral-500 sm:text-2xl sm:leading-7 md:text-4xl md:leading-10 lg:text-6xl lg:leading-15">
+      <h1 className="my-18 text-center text-xl leading-5 font-medium tracking-tighter text-neutral-500 sm:text-2xl sm:leading-7 md:text-4xl md:leading-10 lg:text-6xl lg:leading-15">
         Software Engineer focused on Full-Stack Development. I build scalable
         application and keep learning to grow personally and professionally
       </h1>
