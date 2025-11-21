7be681ef2d8db1a85fdc5e95a86821cf1b766a1b
sunnyJha08 <sunnyjha98971@gmail.com>
2025-11-21 15:24:06 +0530

    feat: add ProjectCard component to Home page for project display

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,9 +1,11 @@
 import HeroSection from "../components/heroSection";
+import Card from "../components/ui/projectCard";
 
 export default function Home() {
   return (
     <div className="mx-auto flex h-auto max-w-7xl flex-col gap-6 p-2">
       <HeroSection />
+      <Card />
     </div>
   );
 }
