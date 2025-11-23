fb95eab45609d8886eeaea80b1bd684b96f62871
sunnyJha08 <sunnyjha98971@gmail.com>
2025-11-23 10:51:51 +0530

    feat: update HeroSection layout for improved structure and readability

--- src/components/heroSection.tsx
+++ src/components/heroSection.tsx
@@ -1,10 +1,10 @@
 export default function HeroSection() {
   return (
-    <>
-      <h1 className="my-18 text-center text-xl leading-5 font-medium tracking-tighter text-neutral-500 sm:text-2xl sm:leading-7 md:text-4xl md:leading-10 lg:text-6xl lg:leading-15">
+    <div className="flex h-[50vh] items-center">
+      <h1 className="text-center text-lg leading-5 font-medium text-neutral-400 md:text-2xl md:leading-7 lg:text-4xl lg:leading-9">
         Software Engineer focused on Full-Stack Development. I build scalable
         application and keep learning to grow personally and professionally
       </h1>
-    </>
+    </div>
   );
 }
