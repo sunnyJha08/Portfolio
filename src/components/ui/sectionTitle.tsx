db44237dd98461b562f1fb1a4ebc409c189d38a2
sunnyJha08 <sunnyjha98971@gmail.com>
2025-11-26 23:43:24 +0530

    feat: added title for very section and created component

---
+++ src/components/ui/sectionTitle.tsx
@@ -0,0 +1,7 @@
+export default function SectionTitle({ text }) {
+  return (
+    <h2 className="text-xl font-medium tracking-tight text-neutral-600 md:text-2xl lg:text-3xl">
+      {text}
+    </h2>
+  );
+}
