9b8c9af1654149e23316b60ef1aa64973db33180
sunnyJha08 <sunnyjha98971@gmail.com>
2025-11-26 23:44:49 +0530

    feat: import and used sectionTitle component in home.tsx

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,11 +1,16 @@
 import HeroSection from "../components/heroSection";
 import Card from "../components/ui/projectCard";
-
+import InfiniteLogosCarousel from "../components/ui/infiniteLogosCarousel";
+import SectionTitle from "../components/ui/sectionTitle";
 export default function Home() {
   return (
     <div className="mx-auto flex h-auto max-w-7xl flex-col gap-6 p-2">
       <HeroSection />
+      <InfiniteLogosCarousel />
+      <SectionTitle text={"Projects"} />
+
       <Card />
+      <SectionTitle text={"Blogs"} />
     </div>
   );
 }
