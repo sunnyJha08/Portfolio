ff03b19ac474e42070cc9cf8f8500e82182cb240
sunnyJha08 <sunnyjha98971@gmail.com>
2025-11-28 22:39:10 +0530

    feat: add profile picture to header and implement Blogs route

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,12 +1,16 @@
 import { NavLink } from "react-router";
-
+import pp from "../assets/images/profilePic.jpg";
 export default function Header() {
   return (
-    <header>
+    <header className="max-w-3xl flex mx-auto items-center justify-between p-4">
       <NavLink to={"/"}>
-        <img src="#" alt="Logo" />
+        <img
+          src={pp}
+          alt="Logo"
+          className="size-12 hover:scale-90 rounded-full object-cover shadow-md"
+        />
       </NavLink>
-      <nav>
+      <nav className="flex gap-4">
         <NavLink to={"/blogs"}>Blogs</NavLink>
         <NavLink to={"/contact"}>Contact</NavLink>
       </nav>
