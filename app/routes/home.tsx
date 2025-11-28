b459576371d6e0e21bd6e65079efcda5e9169c1d
sunnyJha08 <sunnyjha98971@gmail.com>
2025-11-28 20:57:10 +0530

    feat: add initial configuration files and favicon
    - Added favicon.ico to the public directory.
    - Created react-router.config.ts for routing configuration with SSR enabled.
    - Introduced tsconfig.json for TypeScript configuration with strict settings and path aliases.
    - Set up vite.config.ts to integrate React Router, Tailwind CSS, and TypeScript path resolution.

---
+++ app/routes/home.tsx
@@ -0,0 +1,18 @@
+import { Button } from "~/components/ui/button";
+
+import type { Route } from "./+types/home";
+
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: "New React Router App" },
+    { name: "description", content: "Welcome to React Router!" },
+  ];
+}
+
+export default function Home() {
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button>Click me</Button>
+    </div>
+  );
+}
